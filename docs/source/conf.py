1ce282a252ee976d551993d4cad47281c910fbca
Andy <andy@chiefmarley.com>
2013-02-20 21:32:18 -0600

    Added PlantUML plugin
    This requires the install of PlantUML.  It
    allows the generation of uml documents from
    text.  Also requires plantuml sphinx plugin

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,10 +25,13 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ['sphinxcontrib.plantuml']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+plantuml = 'java -jar C:\\\in_path\\\plantuml.jar'
+
 
 # The suffix of source filenames.
 source_suffix = '.rst'
